c3aa4f6f9ce6b487036ef07c4eb4a028bf0e07f5
Robert Leap <robertleap@gmail.com>
2019-11-15 13:32:09 -0800

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-// Serve static assets in production env
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set a static folder
   app.use(express.static('client/build'));
